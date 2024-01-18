b1a3dbcfeef98109905614c1a9f1a94f8a63280d
Abby Swann <aswann@users.noreply.github.com>
2024-01-17 16:24:34 -0800

    added flat10
    updated to add new emissions pathways and additional plots

--- box_model_functions.py
+++ box_model_functions.py
@@ -7,22 +7,67 @@ def co2_emissions(yr, escheme):
 
     ## historical emissions
     time = np.arange(1764, 2006, step=1)
+    spinup_time = np.arange(1, 5001, step=1)
+    ideal_emit_hist = np.array([0] * len(spinup_time))
 
     emit_hist = [0,0.003,0.003,0.003,0.003,0.003,0.003,0.004,0.004,0.004,0.004,0.004,0.004,0.004,0.004,0.004,0.004,
-                0.005,0.005,0.005,0.005,0.005,0.005,0.005,0.005,0.005,0.005,0.006,0.006,0.006,0.006,0.006,0.006,0.007,
-                0.007,0.007,0.008,0.008,0.010,0.009,0.009,0.009,0.010,0.010,0.010,0.010,0.010,0.011,0.011,0.011,0.011,
-        0.012,0.013,0.014,0.014,0.014,0.014,0.014,0.015,0.016,0.016,0.017,0.017,0.018,0.018,0.018,0.024,0.023,
-        0.023,0.024,0.024,0.025,0.029,0.029,0.030,0.031,0.033,0.034,0.036,0.037,0.039,0.043,0.043,0.046,0.047,
-        0.050,0.054,0.054,0.057,0.059,0.069,0.071,0.076,0.077,0.078,0.083,0.091,0.095,0.097,0.104,0.112,0.119,
-        0.122,0.130,0.135,0.142,0.147,0.156,0.173,0.184,0.174,0.188,0.191,0.195,0.196,0.210,0.236,0.243,0.256,
-        0.272,0.275,0.277,0.281,0.295,0.327,0.327,0.356,0.372,0.374,0.370,0.383,0.406,0.419,0.440,0.465,0.507,
-        0.534,0.552,0.566,0.617,0.624,0.663,0.707,0.784,0.750,0.785,0.819,0.836,0.879,0.943,0.850,0.838,0.901,
-        0.955,0.936,0.806,0.932,0.803,0.845,0.970,0.963,0.975,0.983,1.062,1.065,1.145,1.053,0.940,0.847,0.893,
-        0.973,1.027,1.130,1.209,1.142,1.192,1.299,1.334,1.342,1.391,1.383,1.160,1.238,1.392,1.469,1.419,1.630,
-        1.768,1.796,1.841,1.865,2.043,2.178,2.270,2.330,2.462,2.577,2.594,2.700,2.848,3.008,3.145,3.305,3.411,
-        3.588,3.800,4.076,4.231,4.399,4.635,4.644,4.615,4.883,5.029,5.105,5.387,5.332,5.168,5.127,5.110,5.290,
-        5.444,5.610,5.753,5.964,6.089,6.144,6.235,6.118,6.124,6.242,6.372,6.510,6.619,6.588,6.569,6.735,6.896,
-        6.949,7.286,7.672,7.971]
+                 0.005,0.005,0.005,0.005,0.005,0.005,0.005,0.005,0.005,0.005,0.006,0.006,0.006,0.006,0.006,0.006,0.007,
+                 0.007,0.007,0.008,0.008,0.010,0.009,0.009,0.009,0.010,0.010,0.010,0.010,0.010,0.011,0.011,0.011,0.011,
+                 0.012,0.013,0.014,0.014,0.014,0.014,0.014,0.015,0.016,0.016,0.017,0.017,0.018,0.018,0.018,0.024,0.023,
+                 0.023,0.024,0.024,0.025,0.029,0.029,0.030,0.031,0.033,0.034,0.036,0.037,0.039,0.043,0.043,0.046,0.047,
+                 0.050,0.054,0.054,0.057,0.059,0.069,0.071,0.076,0.077,0.078,0.083,0.091,0.095,0.097,0.104,0.112,0.119,
+                 0.122,0.130,0.135,0.142,0.147,0.156,0.173,0.184,0.174,0.188,0.191,0.195,0.196,0.210,0.236,0.243,0.256,
+                 0.272,0.275,0.277,0.281,0.295,0.327,0.327,0.356,0.372,0.374,0.370,0.383,0.406,0.419,0.440,0.465,0.507,
+                 0.534,0.552,0.566,0.617,0.624,0.663,0.707,0.784,0.750,0.785,0.819,0.836,0.879,0.943,0.850,0.838,0.901,
+                 0.955,0.936,0.806,0.932,0.803,0.845,0.970,0.963,0.975,0.983,1.062,1.065,1.145,1.053,0.940,0.847,0.893,
+                 0.973,1.027,1.130,1.209,1.142,1.192,1.299,1.334,1.342,1.391,1.383,1.160,1.238,1.392,1.469,1.419,1.630,
+                 1.768,1.796,1.841,1.865,2.043,2.178,2.270,2.330,2.462,2.577,2.594,2.700,2.848,3.008,3.145,3.305,3.411,
+                 3.588,3.800,4.076,4.231,4.399,4.635,4.644,4.615,4.883,5.029,5.105,5.387,5.332,5.168,5.127,5.110,5.290,
+                 5.444,5.610,5.753,5.964,6.089,6.144,6.235,6.118,6.124,6.242,6.372,6.510,6.619,6.588,6.569,6.735,6.896,
+                 6.949,7.286,7.672,7.971]
+    
+    emit_hist_gcb = [0.72, 0.74, 0.75, 0.77, 0.79, 0.79, 0.81, 0.81, 0.82, 0.84,
+                     0.81, 0.78, 0.78, 0.79, 0.79, 0.78, 0.79, 0.79, 0.79, 0.80,
+                     0.85, 0.89, 0.92, 0.95, 0.95, 0.97, 0.99, 1.00, 1.02, 1.04,
+                     1.08, 1.09, 1.12, 1.15, 1.16, 1.16, 1.17, 1.20, 1.23, 1.24,
+                     1.30, 1.34, 1.36, 1.37, 1.39, 1.43, 1.45, 1.48, 1.51, 1.57,
+                     1.63, 1.66, 1.69, 1.74, 1.77, 1.82, 1.85, 1.96, 1.94, 1.97,
+                     2.01, 2.02, 2.04, 2.11, 2.02, 2.00, 2.05, 2.09, 2.09, 1.95,
+                     2.12, 2.03, 2.09, 2.21, 2.22, 2.25, 2.23, 2.32, 2.32, 2.40,
+                     2.32, 2.22, 2.14, 2.20, 2.29, 2.33, 2.44, 2.52, 2.46, 2.53,
+                     2.69, 2.77, 2.77, 2.80, 2.82, 2.57, 2.68, 2.80, 2.86, 2.80,
+                     3.12, 3.27, 3.33, 3.41, 3.49, 3.72, 3.91, 4.03, 4.15, 4.36,
+                     4.35, 4.24, 4.27, 4.35, 4.45, 4.47, 4.56, 4.69, 4.88, 5.03,
+                     5.32, 5.48, 5.68, 5.88, 5.82, 5.85, 6.09, 6.25, 6.36, 6.46,
+                     6.44, 6.39, 6.36, 6.50, 6.82, 6.93, 7.03, 7.20, 7.41, 7.45,
+                     7.54, 7.64, 7.50, 7.56, 7.71, 7.85, 8.04, 8.57, 8.11, 8.25,
+                     8.34, 8.30, 8.59, 9.09, 9.28, 9.37, 9.73, 9.81, 10.02, 9.99,
+                     10.43, 10.77, 10.87, 10.90, 11.05, 11.17, 10.93, 11.03, 11.19,
+                     11.36, 10.73, 11.21]
+    
+    
+    #         https://c4mip.net/zecmip-protocol
+    bell_curve_1000PgC = [0.20873014, 0.25276203, 0.30488921, 0.3663328, 0.43844296,
+                          0.52270172, 0.62072365, 0.73425378, 0.86516239, 1.01543611,
+                          1.18716509, 1.38252556, 1.6037577, 1.8531385, 2.13294934,
+                          2.44543847, 2.79277839, 3.17701853, 3.60003364, 4.06346858,
+                          4.56868053, 5.11667948, 5.70806844, 6.34298476, 7.0210441,
+                          7.74128883, 8.50214249, 9.30137222, 10.1360608, 11.0025899,
+                          11.8966362, 12.8131814, 13.746537, 14.6903849, 15.6378333,
+                          16.5814888, 17.5135425, 18.4258706, 19.3101466, 20.1579639,
+                          20.9609659, 21.7109814, 22.400162, 23.0211173, 23.5670474,
+                          24.0318658, 24.4103126, 24.6980536, 24.8917628, 24.9891865,
+                          24.9891865, 24.8917628, 24.6980536, 24.4103126, 24.0318658,
+                          23.5670474, 23.0211173, 22.400162, 21.7109814, 20.9609659,
+                          20.1579639, 19.3101466, 18.4258706, 17.5135425, 16.5814888,
+                          15.6378333, 14.6903849, 13.746537, 12.8131814, 11.8966362,
+                          11.0025899, 10.1360608, 9.30137222, 8.50214249, 7.74128883,
+                          7.0210441, 6.34298476, 5.70806844, 5.11667948, 4.56868053,
+                          4.06346858, 3.60003364, 3.17701853, 2.79277839, 2.44543847,
+                          2.13294934, 1.8531385, 1.6037577, 1.38252556, 1.18716509,
+                          1.01543611, 0.86516239, 0.73425378, 0.62072365, 0.52270172,
+                          0.43844296, 0.3663328, 0.30488921, 0.25276203, 0.20873014]
+    
 
     if escheme == "rcp85":
         time2 = np.arange(2006, 2101, step=1)
@@ -37,29 +82,122 @@ def co2_emissions(yr, escheme):
         28.377,28.417,28.458,28.498,28.538,28.579,28.619,28.659,28.700,28.740]
 
         emit = np.concatenate([emit_hist, emit_future])
-    elif escheme == "pulse":
-        time = time.transpose()
-        emit = time * 0
-        emit[time == 1800] = 10 # single year of pulsed emissions 
+    
+    elif escheme == "hist_gcb":
+        time_future = np.arange(0, 1000, step=1)
+        time = np.concatenate([spinup_time, time_future+spinup_time[-1]])
+
+        emit_future = np.zeros(len(time_future))
+        emit_future[0:172] = emit_hist_gcb
+        emit = np.concatenate([ideal_emit_hist, emit_future])
+
+    elif escheme == "pulse": # done
+        time_future = np.arange(1, 1001, step=1)
+        time = np.concatenate([spinup_time, time_future+spinup_time[-1]])
+
+        emit_future = np.zeros(len(time_future))
+        emit_future[0] = 1000
+        emit = np.concatenate([ideal_emit_hist, emit_future])
+        
+    elif escheme == "pulse500": # done
+        time_future = np.arange(1, 1001, step=1)
+        time = np.concatenate([spinup_time, time_future+spinup_time[-1]])
+
+        emit_future = np.zeros(len(time_future))
+        emit_future[0] = 500
+        emit = np.concatenate([ideal_emit_hist, emit_future])
+        
+    elif escheme == "flat10_zec": # done
+        time_future = np.arange(1, 1001, step=1)
+        time = np.concatenate([spinup_time, time_future+spinup_time[-1]])
+
+        emit_future = np.zeros(len(time_future))
+        emit_future[0:100] = 10
+        emit = np.concatenate([ideal_emit_hist, emit_future])
+#         emit = emit_future
+#         time = time_future
+        
+    elif escheme == "CDR-pi-pulse":
+        time_future = np.arange(1, 1001, step=1)
+        time = np.concatenate([spinup_time, time_future+spinup_time[-1]])
+        
+        emit_future = np.zeros(len(time_future))
+        emit_future[0] = 1000
+        emit = np.concatenate([ideal_emit_hist, emit_future])
+
+    elif escheme == "pulse-reversal": #done
+        time_future = np.arange(1, 1001, step=1)
+        time = np.concatenate([spinup_time, time_future+len(spinup_time)])
+
+        emit_future = np.zeros(len(time_future))
+        emit_future[0] = 100
+        emit_future[100] = -100
+        emit = np.concatenate([ideal_emit_hist, emit_future])
+    
+    elif escheme == "esm-bell-1000PgC":
+        time_future = np.arange(1, 1001, step=1)
+        time = np.concatenate([spinup_time, time_future+len(spinup_time)])
+
+        emit_future = np.zeros(len(time_future))                
+        emit_future[0:100] = bell_curve_1000PgC
+        emit = np.concatenate([ideal_emit_hist, emit_future])
+
+    elif escheme == "esm-restoration": #HAVE NOT EDITED THIS ONE
+        time_future = np.arange(1, 1001, step=1)
+        time = np.concatenate([spinup_time, time_future+len(spinup_time)])
+        
+        emit_future = np.zeros(len(time_future)) 
+        emit_future[0:99] = np.diff(bell_curve_1000PgC)
+        emit = np.concatenate([ideal_emit_hist, emit_future])
+        
+    elif escheme == "CDR-pi-pulse-500":
+        time_future = np.arange(1, 1001, step=1)
+        time = np.concatenate([spinup_time, time_future+len(spinup_time)])
+        
+        emit_future = np.zeros(len(time_future))
+        emit_future[0] = 500
+        emit = np.concatenate([ideal_emit_hist, emit_future])
+        
+    elif escheme == "pulse-reversal-500-100":
+        time_future = np.arange(1, 1001, step=1)
+        time = np.concatenate([spinup_time, time_future+len(spinup_time)])
+        
+        emit_future = np.zeros(len(time_future))
+        emit_future[0] = 500
+        emit_future[100] = -100
+        emit = np.concatenate([ideal_emit_hist, emit_future])
+
+    elif escheme == "pulse-reversal-500-500":
+        time_future = np.arange(1, 1001, step=1)
+        time = np.concatenate([spinup_time, time_future+len(spinup_time)])
+        
+        emit_future = np.zeros(len(time_future))
+        emit_future[0] = 500
+        emit_future[100] = -500
+        emit = np.concatenate([ideal_emit_hist, emit_future])
+
     else:
-        time = time.transpose()
-        emit = time * 0
+        print('unspecified emissions scenario')
 
-    #time = [-1e6, time, 1e6]
-    # time = np.array([-1e6, time, 1e6])
-    time = np.insert(time, 0, -1e6, axis=0)
-    time = np.append(time, 1e6)
+# GEMS commented
+#     time = [-1e6, time, 1e6]
+#     time = np.array([-1e6, time, 1e6])
+    time = np.insert(time, 0, -1e6) #GEMS
+    time = np.append(time, 1e6) #GEMS
 
-    # emit = [0, emit, emit[-1]]
-    emit = np.insert(emit, 0, 0, axis=0)
-    emit = np.append(emit, emit[-1])
+# #     emit = [0, emit, emit[-1]]
+    emit = np.insert(emit, 0, 0) #GEMS
+    emit = np.append(emit, emit[-1]) #GEMS
+# GEMS commented
 
 
     # FF=interp1(time,emit,yr);         
     #FF = interp1d(time, emit, yr)
-    FF_fctn = interp1d(time, emit)
-    FF = FF_fctn(yr)
+#     FF = np.interp(yr, time, emit) #GEMS
+    FF_fctn = interp1d(time, emit) #GEMS commented
+    FF = FF_fctn(yr) #GEMS commented
 
+#     return(FF)
     return(FF)
 
 def calc_pco2(t, s, ta, c, phg):
@@ -253,7 +391,7 @@ def carbon_climate_derivs(t, y, PE, PS, PL, PO):
     # python: yn_py2 = yn_f2(xn)
 
     # atmosphere + climate
-    FF = co2_emissions(ycal, PS['escheme']) # fossil fuel co2 emissions (Pg/yr)
+    FF = co2_emissions(ycal, PS['escheme']) # fossil fuel co2 emissions (Pg/yr)	
     # [ycal FF]
     FF = FF * 1e15 / 12 / PE['spery'] # convert to molC/s
     RFco2 = 5.35 * np.log(patm / PE['patm0']) * PS['DoRadCO2'] # radiative forcing from CO2
